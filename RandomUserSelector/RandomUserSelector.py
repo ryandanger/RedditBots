91234438c180af655713a49effd4feb1dbd57771
Tristan Hanson <chaoscreative1@gmail.com>
2016-04-19 23:17:39 -0500

    adds kickbot and stuff
    needs meticulous troubleshooting

--- RandomUserSelector/RandomUserSelector.py
+++ RandomUserSelector/RandomUserSelector.py
@@ -13,19 +13,19 @@ import OAuth2Util
 # ### USER CONFIGURATION ### #
 
 # The bot's useragent. It should contain a short description of what it does and your username. e.g. "RSS Bot by /u/SmBe19"
-USERAGENT = "424ae4106a3f6fe90e6c"
+USERAGENT = "klol"
 
 # The name of the subreddit to post to. e.g. "funny"
-SUBREDDIT = "apsconditum"
+SUBREDDIT = "waiaas"
 
-# Number of users to select
-USERS_COUNT = 33
+# Number of users to select -- should be dependent on the kick users section below, so no longer a constant/param
+#USERS_COUNT = 33
 
 # Number of comments from which the users are selected (max is 1000)
 SAMPLE_SIZE = 1000
 
 # Whether to check whether the selected user is already a contributor (does not work atm)
-CHECK_CONTRIBUTOR = False
+CHECK_CONTRIBUTOR = True
 
 # Whether to add the selected users as approved submitters.
 # Note that that running the script with this flag set to True is considered spam.
@@ -49,16 +49,22 @@ def run_bot():
 		o.refresh()
 	sub = r.get_subreddit(SUBREDDIT)
 
-	print("Start bot for subreddit", SUBREDDIT)
-	print("Select", USERS_COUNT, "users from", SAMPLE_SIZE, "comments")
+	users_count = 0
 
-	sub = r.get_subreddit(SUBREDDIT)
-	if CHECK_CONTRIBUTOR:
-		contributors = list(sub.get_contributors())
+	# kick peeps (in theory)
+	contributors = list(sub.get_contributors())
+	for contributor in contributors:
+		contributor_comments = list(contributor.get_comments(SUBREDDIT, sort='new',t='week', limit=None))
+		if not contributor_comments:
+			contributor.leave_contributor(SUBREDDIT)
+			users_count += 1
+
+	print("Start bot for subreddit", SUBREDDIT)
+	print("Select", users_count, "users from", SAMPLE_SIZE, "comments")
 	comments = list(r.get_comments("all", limit=SAMPLE_SIZE))
 	added_users = []
 
-	for i in range(USERS_COUNT):
+	for i in range(users_count):
 		user = random.choice(comments).author
 		while (CHECK_CONTRIBUTOR and user in contributors) or user in added_users:
 			user = random.choice(comments).author
